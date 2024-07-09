4d5daa44182aa82a29c43c367fa282f48621f4e4
Abel Merete Habteyohannes <abelmerete@yahoo.com>
2024-07-09 23:49:49 +0300

    modify: add comment

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -6,6 +6,8 @@ final emailRegex = RegExp(r"^[A-Za-z0-9.+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
 final nameRegex = RegExp(r"^[A-Za-z]{1,50}$");
 final phoneRegex = RegExp(r"^(?:\+251[79]\d{8})$");
 
+
+// firebase related
 final auth = FirebaseAuth.instance;
 final db = FirebaseFirestore.instance;
 final storage = FirebaseStorage.instance;
